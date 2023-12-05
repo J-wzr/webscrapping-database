dfaee906b0fc1418686c1d457a6828fe90f756e7
J-wzr <joshuamwesigwa58@gmail.com>
2023-12-05 23:15:30 +0300

    added the oop style to improve the robustness of the application

--- main.py
+++ main.py
@@ -6,65 +6,75 @@ import selectorlib
 import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
-connection = sqlite3.connect("data.db")
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-def scrape(url):
-    request = requests.get(url, headers=HEADERS)
-    source = request.text
-    return source
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Event:
 
+    def scrape(self, url):
+        request = requests.get(url, headers=HEADERS)
+        source = request.text
+        return source
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
-    SENDER = "your email"
-    PASSWORD = "your password"
-    RECEIVER = "receiver email"
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(SENDER, PASSWORD)
-        server.sendmail(SENDER, RECEIVER, message)
-
-    print("Email Sent")
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
+class Email:
+
+    def send(message):
+        host = "smtp.gmail.com"
+        port = 465
+        SENDER = "your email"
+        PASSWORD = "your password"
+        RECEIVER = "receiver email"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(SENDER, PASSWORD)
+            server.sendmail(SENDER, RECEIVER, message)
+
+        print("Email Sent")
 
 
+class Database:
 
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events")
-    rows = cursor.fetchall()
-    return rows
+    def __init__(self, database_path) -> None:
+        self.connection = sqlite3.connect(database_path)
+
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
+
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events")
+        rows = cursor.fetchall()
+        return rows
 
 
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
         
         
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message="Hey, A new event was found!")
+                database.store(extracted)
+                email = Email()
+                email.send(message="Hey, A new event was found!")
             time.sleep(1)
